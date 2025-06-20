fc57d3dc988f0741d29d5a2f815bf08f3b7f1887
Denny <dennyye32@gmail.com>
2025-06-20 18:56:25 -0400

    Updated small

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -66,7 +66,7 @@ export const Navbar = () => {
           {isMenuOpen ? (
             <X size={24} className="cursor-pointer" />
           ) : (
-            <Menu size={24} />
+            <Menu size={24} className="cursor-pointer" />
           )}
         </button>
 
